14f806466998e80a0d1ae7405ef4b51172136904
Maria-Grassi <77647573+Maria-Grassi@users.noreply.github.com>
2021-01-19 18:09:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+let par;
+for( let i=0; i<7; i++ ){
+    if( i%2==0){ 
+        console.log(dias[i])+ ", "}
+    if (i==6){
+        prompt("¿Hoy que se hace?")}
+}
